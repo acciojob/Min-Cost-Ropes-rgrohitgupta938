e851e7dc84429a85923c8c13fd2725f798f3192b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-02-11 19:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function mincost(arr) {
+  if (arr.length <= 1) return 0;
+
   arr.sort((a, b) => a - b);
-  let sum = arr.reduce((acc, curr, index) => acc + index, 0);
-  return sum;
+
+  let totalCost = 0;
+  while (arr.length > 1) {
+    const rope1 = arr.shift();
+    const rope2 = arr.shift();
+    const cost = rope1 + rope2;
+    totalCost += cost;
+    arr.push(cost);
+    arr.sort((a, b) => a - b);
+  }
+
+  return totalCost;
 }
+
 
 module.exports=mincost;
